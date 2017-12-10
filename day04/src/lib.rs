02ed569a74016f6405d7f1c9f5d7c9a8c85923d4
Tomas Cernaj <tomas@cernaj.de>
2017-12-10 19:08:05 +0100

    Day 4: Part 2

--- day04/src/lib.rs
+++ day04/src/lib.rs
@@ -7,7 +7,9 @@ pub fn is_valid_passphrase<T: Deref<Target = str>>(input: T) -> bool {
     let mut words = HashMap::new();
 
     for word in phrase.split_whitespace() {
-        *words.entry(word).or_insert(0) += 1;
+        let mut chars: Vec<char> = word.chars().collect();
+        chars.sort();
+        *words.entry(chars).or_insert(0) += 1;
     }
 
     !words.is_empty() && words.values().find(|c| **c > 1).is_none()
@@ -19,9 +21,10 @@ mod tests {
 
     #[test]
     fn it_works() {
-        assert_eq!(false, is_valid_passphrase(""));
-        assert_eq!(true, is_valid_passphrase("aa bb cc dd ee"));
-        assert_eq!(false, is_valid_passphrase("aa bb cc dd aa"));
-        assert_eq!(true, is_valid_passphrase("aa bb cc dd aaa"));
+        assert_eq!(true, is_valid_passphrase("abcde fghij"));
+        assert_eq!(false, is_valid_passphrase("abcde xyz ecdab"));
+        assert_eq!(true, is_valid_passphrase("a ab abc abd abf abj"));
+        assert_eq!(true, is_valid_passphrase("iiii oiii ooii oooi oooo"));
+        assert_eq!(false, is_valid_passphrase("oiii ioii iioi iiio"));
     }
 }

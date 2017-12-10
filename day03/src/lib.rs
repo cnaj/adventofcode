c1d6a27dfa4e271de8f7a76a5d190ff0eab616be
Tomas Cernaj <tomas@cernaj.de>
2017-12-10 14:34:00 +0100

    Day 3: Rewrite of part 1 after seeing part 2

--- day03/src/lib.rs
+++ day03/src/lib.rs
@@ -1,21 +1,49 @@
-pub fn compute(n: u32) -> u32 {
-    let square_nr = find_square_nr(n);
-
-    if square_nr == 0 {
-        return 0;
-    }
-
-    let lower_right_prev = lower_right(square_nr - 1);
-    let side_len = square_nr * 2;
-
-    let pos_on_side = (n - lower_right_prev - 1) % side_len + 1;
-    let middle_of_side = square_nr;
-    let steps_to_mid = diff(pos_on_side, middle_of_side);
-
-    steps_to_mid + square_nr
+pub fn compute(n: u32) -> i32 {
+    let (x, y) = find_grid_pos(n);
+    x.abs() + y.abs()
 }
 
-fn find_square_nr(n: u32) -> u32 {
+/// Returns the position of the number on the grid, with the access port at position (0, 0).
+///
+/// # Examples
+///
+/// ```
+/// use day03::find_grid_pos;
+///
+/// assert_eq!((0, 0), find_grid_pos(1));
+/// assert_eq!((0, -1), find_grid_pos(8));
+/// assert_eq!((1, -1), find_grid_pos(9));
+/// assert_eq!((2, -1), find_grid_pos(10));
+/// assert_eq!((2, 1), find_grid_pos(12));
+/// assert_eq!((2, -2), find_grid_pos(25));
+/// assert_eq!((-2, -1), find_grid_pos(20));
+/// assert_eq!((0, -2), find_grid_pos(23));
+/// ```
+pub fn find_grid_pos(n: u32) -> (i32, i32) {
+    let ring_nr = find_ring_nr(n);
+    if ring_nr == 0 {
+        return (0, 0);
+    }
+
+    let lower_right_prev = lower_right(ring_nr - 1);
+    let side_len = ring_nr * 2;
+
+    let n_on_ring = n - lower_right_prev - 1;
+
+    let pos_on_side = (n_on_ring % side_len) + 1;
+    let middle_of_side = ring_nr as i32;
+    let side_delta = pos_on_side as i32 - middle_of_side;
+
+    match n_on_ring / side_len {
+        0 => (middle_of_side, side_delta),
+        1 => (-side_delta, middle_of_side),
+        2 => (-middle_of_side, -side_delta),
+        3 => (side_delta, -middle_of_side),
+        _ => panic!("unexpected"),
+    }
+}
+
+fn find_ring_nr(n: u32) -> u32 {
     if n == 0 {
         panic!("n must be greater than 0, was {}", n);
     }
@@ -30,7 +58,7 @@ fn find_square_nr(n: u32) -> u32 {
     step
 }
 
-/// Returns the lower-right number of a square. Squares are counted starting from 0 outwards.
+/// Returns the lower-right number of a ring. Rings are counted starting from 0 outwards.
 ///
 /// # Examples
 ///
@@ -41,16 +69,8 @@ fn find_square_nr(n: u32) -> u32 {
 /// assert_eq!(9, lower_right(1));
 /// assert_eq!(25, lower_right(2));
 /// ```
-pub fn lower_right(square_nr: u32) -> u32 {
-    (square_nr * 2 + 1).pow(2)
-}
-
-fn diff(a: u32, b: u32) -> u32 {
-    if a < b {
-        b - a
-    } else {
-        a - b
-    }
+pub fn lower_right(ring_nr: u32) -> u32 {
+    (ring_nr * 2 + 1).pow(2)
 }
 
 #[cfg(test)]
@@ -78,12 +98,12 @@ mod tests {
     }
 
     #[test]
-    fn test_find_square_nr() {
-        assert_eq!(0, find_square_nr(1));
-        assert_eq!(1, find_square_nr(9));
-        assert_eq!(2, find_square_nr(25));
+    fn test_find_ring_nr() {
+        assert_eq!(0, find_ring_nr(1));
+        assert_eq!(1, find_ring_nr(9));
+        assert_eq!(2, find_ring_nr(25));
 
-        assert_eq!(1, find_square_nr(8));
-        assert_eq!(2, find_square_nr(20));
+        assert_eq!(1, find_ring_nr(8));
+        assert_eq!(2, find_ring_nr(20));
     }
 }

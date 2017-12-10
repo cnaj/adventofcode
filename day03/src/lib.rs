56a6182153753daa826360c811b260d28eb7996c
Tomas Cernaj <tomas@cernaj.de>
2017-12-10 16:37:10 +0100

    Day 3: Implement find_n(x, y)

--- day03/src/lib.rs
+++ day03/src/lib.rs
@@ -3,6 +3,53 @@ pub fn compute(n: u32) -> i32 {
     x.abs() + y.abs()
 }
 
+/// Returns the element number of the given position, starting with 1 at (0, 0).
+///
+/// # Examples
+///
+/// ```
+/// use day03::find_n;
+///
+/// assert_eq!( 1, find_n( 0,  0));
+/// assert_eq!( 8, find_n( 0, -1));
+/// assert_eq!( 9, find_n( 1, -1));
+/// assert_eq!(10, find_n( 2, -1));
+/// assert_eq!(12, find_n( 2,  1));
+/// assert_eq!(25, find_n( 2, -2));
+/// assert_eq!(20, find_n(-2, -1));
+/// assert_eq!(23, find_n( 0, -2));
+/// ```
+pub fn find_n(x: i32, y: i32) -> u32 {
+    if (x, y) == (0, 0) {
+        return 1;
+    }
+
+    let ring;
+    let pos_on_ring;
+    if x >= y && x > -y {
+        // side 0
+        ring = x as u32;
+        pos_on_ring = (x + y - 1) as u32;
+    } else if -x > -y && x >= -y {
+        // side 1
+        ring = y as u32;
+        pos_on_ring = (y - x - 1) as u32 + ring * 2;
+    } else if -x > y && -x >= -y {
+        // side 2
+        ring = -x as u32;
+        pos_on_ring = (-x - y - 1) as u32 + ring * 4;
+    } else {
+        // side 3
+        ring = -y as u32;
+        pos_on_ring = (x + - y - 1) as u32 + ring * 6;
+    }
+
+    // (2r + 1)² - (2r - 1)² = 8r
+    let total_on_ring = 8 * ring;
+
+    lower_right(ring) + 1 - total_on_ring + pos_on_ring
+}
+
 /// Returns the position of the number on the grid, with the access port at position (0, 0).
 ///
 /// # Examples
@@ -10,28 +57,28 @@ pub fn compute(n: u32) -> i32 {
 /// ```
 /// use day03::find_grid_pos;
 ///
-/// assert_eq!((0, 0), find_grid_pos(1));
-/// assert_eq!((0, -1), find_grid_pos(8));
-/// assert_eq!((1, -1), find_grid_pos(9));
-/// assert_eq!((2, -1), find_grid_pos(10));
-/// assert_eq!((2, 1), find_grid_pos(12));
-/// assert_eq!((2, -2), find_grid_pos(25));
+/// assert_eq!(( 0,  0), find_grid_pos( 1));
+/// assert_eq!(( 0, -1), find_grid_pos( 8));
+/// assert_eq!(( 1, -1), find_grid_pos( 9));
+/// assert_eq!(( 2, -1), find_grid_pos(10));
+/// assert_eq!(( 2,  1), find_grid_pos(12));
+/// assert_eq!(( 2, -2), find_grid_pos(25));
 /// assert_eq!((-2, -1), find_grid_pos(20));
-/// assert_eq!((0, -2), find_grid_pos(23));
+/// assert_eq!(( 0, -2), find_grid_pos(23));
 /// ```
 pub fn find_grid_pos(n: u32) -> (i32, i32) {
-    let ring_nr = find_ring_nr(n);
-    if ring_nr == 0 {
+    let ring = find_ring(n);
+    if ring == 0 {
         return (0, 0);
     }
 
-    let lower_right_prev = lower_right(ring_nr - 1);
-    let side_len = ring_nr * 2;
+    let lower_right_prev = lower_right(ring - 1);
+    let side_len = ring * 2;
 
     let n_on_ring = n - lower_right_prev - 1;
 
     let pos_on_side = (n_on_ring % side_len) + 1;
-    let middle_of_side = ring_nr as i32;
+    let middle_of_side = ring as i32;
     let side_delta = pos_on_side as i32 - middle_of_side;
 
     match n_on_ring / side_len {
@@ -43,7 +90,7 @@ pub fn find_grid_pos(n: u32) -> (i32, i32) {
     }
 }
 
-fn find_ring_nr(n: u32) -> u32 {
+fn find_ring(n: u32) -> u32 {
     if n == 0 {
         panic!("n must be greater than 0, was {}", n);
     }
@@ -99,11 +146,11 @@ mod tests {
 
     #[test]
     fn test_find_ring_nr() {
-        assert_eq!(0, find_ring_nr(1));
-        assert_eq!(1, find_ring_nr(9));
-        assert_eq!(2, find_ring_nr(25));
+        assert_eq!(0, find_ring(1));
+        assert_eq!(1, find_ring(9));
+        assert_eq!(2, find_ring(25));
 
-        assert_eq!(1, find_ring_nr(8));
-        assert_eq!(2, find_ring_nr(20));
+        assert_eq!(1, find_ring(8));
+        assert_eq!(2, find_ring(20));
     }
 }

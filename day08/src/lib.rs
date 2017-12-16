13b20e7984b136440849383707692325f121597a
Tomas Cernaj <tomas@cernaj.de>
2017-12-16 23:43:09 +0000

    Day 8: Part 1

---
+++ day08/src/lib.rs
@@ -0,0 +1,92 @@
+#![allow(dead_code)]
+
+#[macro_use] extern crate lazy_static;
+extern crate regex;
+
+use std::collections::HashMap;
+
+use regex::Regex;
+
+pub struct ProgramExecutor {
+    registers: HashMap<String, i32>,
+}
+
+impl ProgramExecutor {
+
+    pub fn new() -> ProgramExecutor {
+        ProgramExecutor {
+            registers: HashMap::new(),
+        }
+    }
+
+    pub fn add_instruction(&mut self, line: &str) -> Result<(), String> {
+        lazy_static! {
+            static ref RE: Regex = Regex::new(
+                r"^(\w+) (inc|dec) (-?\d+) if (\w+) (<|<=|>|>=|==|!=) (-?\d+)"
+            ).unwrap();
+        }
+
+        RE.captures(line)
+            .map_or_else(
+                || Err(format!("invalid input line: {}", line)),
+                |cpt| {
+                    if self.test(&cpt[4], &cpt[5],
+                                 i32::from_str_radix(&cpt[6], 10).unwrap()) {
+                        self.execute(&cpt[1], &cpt[2],
+                                     i32::from_str_radix(&cpt[3], 10).unwrap());
+                    }
+                    Ok(())
+                },
+            )
+    }
+
+    pub fn get_largest_register_value(&self) -> Option<i32> {
+        self.registers.values()
+            .max()
+            .map(|i| *i)
+    }
+
+    fn test(&mut self, reg: &str, cond: &str, value: i32) -> bool {
+        let reg_value = *self.registers.get(reg).unwrap_or(&0);
+        match cond {
+            "<" => reg_value < value,
+            "<=" => reg_value <= value,
+            ">" => reg_value > value,
+            ">=" => reg_value >= value,
+            "==" => reg_value == value,
+            "!=" => reg_value != value,
+            _ => panic!("unexpected instruction {}", cond),
+        }
+    }
+
+    fn execute(&mut self, reg: &str, op: &str, amount: i32) {
+        let reg_value = *self.registers.get(reg).unwrap_or(&0);
+        let new_value = match op {
+            "inc" => reg_value + amount,
+            "dec" => reg_value - amount,
+            _ => panic!("unexpected instruction {}", op),
+        };
+        self.registers.insert(reg.to_owned(), new_value);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let lines = [
+            "b inc 5 if a > 1",
+            "a inc 1 if b < 5",
+            "c dec -10 if a >= 1",
+            "c inc -20 if c == 10",
+        ];
+
+        let mut executor = ProgramExecutor::new();
+        lines.iter()
+            .for_each(|line| executor.add_instruction(&line).unwrap());
+
+        assert_eq!(Some(1), executor.get_largest_register_value());
+    }
+}
---
+++ day08/src/main.rs
@@ -0,0 +1,36 @@
+extern crate day08;
+
+use std::io;
+use std::io::BufRead;
+
+use day08::ProgramExecutor;
+
+fn main() {
+    let stdin = io::stdin();
+    let handle = stdin.lock();
+
+    match compute(handle) {
+        Ok(result) => {
+            if let Some(result) = result {
+                println!("{}", result)
+            }
+        },
+        Err(err) => {
+            eprintln!("Error: {}", err);
+        },
+    }
+}
+
+fn compute(handle: io::StdinLock) -> Result<Option<i32>, String> {
+    let mut executor = ProgramExecutor::new();
+
+    let lines = handle.lines();
+    for line_result in lines {
+        match line_result {
+            Ok(line) => executor.add_instruction(&line)?,
+            Err(err) => return Err(format!("Could not read line: {}", err)),
+        };
+    }
+
+    Ok(executor.get_largest_register_value())
+}

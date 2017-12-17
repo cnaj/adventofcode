9876c6e3d2c4122d20c9abf8ab2e5a42966a1a9f
Tomas Cernaj <tomas@cernaj.de>
2017-12-17 18:56:13 +0000

    Day 10: Refactoring after seeing part 2

--- day10/src/lib.rs
+++ day10/src/lib.rs
@@ -1,51 +1,46 @@
-pub fn knot_hash(list: &mut [u8], lengths: &str) -> u16 {
-    if list.len() < 2 {
-        panic!("list must have at least 2 elements");
-    }
+pub struct KnotHash {
+    list: [u8; 256],
+}
 
-    let mut cur = 0u8;
-    for el in list.iter_mut() {
-        *el = cur;
-        cur = cur.wrapping_add(1);
-    }
+impl KnotHash {
+    pub fn hash(lengths: &str) -> KnotHash {
+        let mut list = [0u8; 256];
 
-    let mut pos = 0usize;
-    let mut skip = 0usize;
-    for len in lengths.split(',')
-        .map(|s| usize::from_str_radix(s, 10).unwrap()) {
-
-        if len > 0 {
-            let mut l: usize = pos;
-            let mut r: usize = pos + len - 1;
-            if r >= list.len() {
-                r = r % list.len();
-            }
-
-            for _ in 0usize..len / 2 {
-                let tmp: u8 = list[l];
-                list[l] = list[r];
-                list[r] = tmp;
-
-                l += 1;
-                if l == list.len() {
-                    l = 0;
-                }
-                if r == 0 {
-                    r = list.len() - 1;
-                } else {
-                    r -= 1;
-                }
-            }
+        let mut cur = 0u8;
+        for el in list.iter_mut() {
+            *el = cur;
+            cur = cur.wrapping_add(1);
         }
 
-        pos += len + skip;
-        if pos >= list.len() {
-            pos = pos % list.len();
+        let mut pos = 0usize;
+        let mut skip = 0usize;
+        for len in lengths.split(',')
+            .map(|s| usize::from_str_radix(s, 10).unwrap()) {
+
+            if len > 0 {
+                let mut l: usize = pos;
+                let mut r: usize = (pos + len - 1) & 0xff;
+
+                for _ in 0usize..len / 2 {
+                    let tmp: u8 = list[l];
+                    list[l] = list[r];
+                    list[r] = tmp;
+
+                    l = (l + 1) & 0xff;
+                    r = (r + 0xff) & 0xff;
+                }
+            }
+
+            pos = (pos + len + skip) & 0xff;
+            skip = (skip + 1) & 0xff;
         }
-        skip += 1;
+
+        KnotHash { list }
     }
 
-    list[0] as u16 * list[1] as u16
+    pub fn checksum(&self) -> u16 {
+        self.list[0] as u16 * self.list[1] as u16
+    }
 }
 
 #[cfg(test)]
@@ -53,14 +48,8 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let mut list = [0u8; 5];
-        assert_eq!(12, knot_hash(&mut list, "3,4,1,5"));
-    }
-
-    #[test]
     fn puzzle() {
-        let mut list = [0u8; 256];
-        assert_eq!(15990, knot_hash(&mut list, "183,0,31,146,254,240,223,150,2,206,161,1,255,232,199,88"));
+        let hash = KnotHash::hash("183,0,31,146,254,240,223,150,2,206,161,1,255,232,199,88");
+        assert_eq!(15990, hash.checksum());
     }
 }
